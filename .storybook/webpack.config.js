d43056ecefd88bdc16c9b8a167fa84cf32771618
trevorfunk <trevorjonfunk@gmail.com>
2022-09-11 16:36:21 -0700

    build button and render styles

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,25 @@
 const path = require("path");
+const htmlWebpack = require("html-webpack-plugin");
 
 module.exports = async ({ config, mode }) => {
   return {
     ...config,
+    // module: {
+    //   ...config.module,
+    //   rules: [
+    //     ...config.module.rules,
+    //     {
+    //       test: /\.(css|scss|sass)$/,
+    //       use: ["style-loader", "css-loader",
+    //         "sass-loader"
+    //       ]
+    //     }
+    //   ]
+    // },
+
     resolve: {
-      ...config.resolve,
-      modules: [path.resolve(__dirname, "../src"), ...config.resolve.modules]
-    }
+          ...config.resolve,
+          modules: [path.resolve(__dirname, "../src"), ...config.resolve.modules]
+        }
   };
-};
+  };
